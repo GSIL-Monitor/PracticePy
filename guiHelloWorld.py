0a4983ffa179f354a48825120c4cfadf6aef564e
fmeng <512537531@qq.com>
2017-03-29 00:27:34 +0800

    gui演示

---
+++ guiHelloWorld.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 00:20:46 2017
+
+@author: MFX
+"""
+
+#==============================================================================
+# from Tkinter import *
+# 
+# class Application(Frame):
+#     def __init__(self, master=None):
+#         Frame.__init__(self, master)
+#         self.pack()
+#         self.createWidgets()
+# 
+#     def createWidgets(self):
+#         self.helloLabel = Label(self, text='Hello, world!')
+#         self.helloLabel.pack()
+#         self.quitButton = Button(self, text='Quit', command=self.quit)
+#         self.quitButton.pack()
+#==============================================================================
+        
+from Tkinter import *
+import tkMessageBox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        tkMessageBox.showinfo('Message', 'Hello, %s' % name)
+
+app = Application()
+# 设置窗口标题:
+app.master.title('Hello World')
+# 主消息循环:
+app.mainloop()
+
